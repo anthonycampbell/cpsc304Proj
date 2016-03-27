f9d9eb6c14e4c963b09689641cbaba37ab114730
Weijun Qiu <weijun.q@gmail.com>
2016-03-27 16:22:21 -0700

    saving progress'

--- project.sql
+++ project.sql
@@ -11,14 +11,14 @@ drop table has_premium_lounge_in;
 drop table on_board;
 
 create table member_companies
-       (aa_name varchar(20),
-       ac_name varchar(20),
+       (aa_name varchar(20) not null,
+       ac_name varchar(20) not null,
        primary key (aa_name, ac_name));
 
 grant select on member_companies to public;
 
 create table airline_companies
-       (ac_name varchar(20),
+       (ac_name varchar(20) not null,
        location   varchar(20),
        primary key (ac_name));
 
@@ -27,65 +27,76 @@ grant select on airline_companies to public;
 create table airliner_oo1
        (flight# char(5),
        model# char(5),
-       ac_name varchar(20),
-       to_airport_code char(3),
-       from_airport_code char(3),
-       primary key (flight#));
+       ac_name varchar(20) not null,
+       to_airport_code char(3) not null,
+       from_airport_code char(3) not null,
+       primary key (flight#),
+       foreign key (ac_name) references airline_companies,
+       foreign key (to_airport_code, from_airport_code) references airport);
 
 grant select on airliner_oo1 to public;
 
 create table airliner_oo2
-       (flight# char(5),
-       departure_time date,
-       arrival_time date,
+       (flight# char(5) not null,
+       departure_time date not null,
+       arrival_time date not null,
        primary key (flight#, arrival_time));
 
 grant select on airliner_oo2 to public;
 
 create table air_alliances
-       (aa_name varchar(20),
+       (aa_name varchar(20) not null,
        primary key (aa_name));
 
 grant select on air_alliances to public;
 
 create table airports
-       (airport_code varchar(20),
+       (airport_code char(3) not null,
        primary key (airport_code));
 
 grant select on airports to public;
 
 create table isMember
-       (aa_name varchar(20),
-       passport# int,
-       primary key (aa_name, passport#));
+       (aa_name varchar(20) not null,
+       passport# int not null,
+       primary key (aa_name, passport#),
+       foreign key (passport#) references passengers,
+       foreign key (aa_name) references air_alliances);
 
 grant select on isMember to public;
 
 create table passengers
-       (passport# int,
+       (passport# int not null,
        primary key (passport#));
 
 grant select on passengers to public;
 
 create table from_to
-       (from_airport_code varchar(20),
-       to_airport_code varchar(20),
-       primary key (from_airport_code, to_airport_code));
+       (from_airport_code char(3) not null,
+       to_airport_code char(3) not null,
+       primary key (from_airport_code, to_airport_code),
+       foreign key (from_airport_code, to_airport_code) references airports);
 
 grant select on from_to to public;
 
 create table has_premium_lounge_in
-       (airport_code char(3),
+       (airport_code char(3) not null,
        aa_name varchar(20),
-       primary key (airport_code));
+       primary key (airport_code),
+       foreign key (airport_code) references airports,
+       foreign key (aa_name) references air_alliances);
 
 grant select on has_premium_lounge_in to public;
 
 create table on_board
-       (passport# int,
-       flight# int,
-       departure_time date,
-       destination varchar(20),
-       primary key (passport#, departure_time));
+       (passport# int not null,
+       flight# char(5) not null,
+       departure_time date not null,
+       destination char(3),
+       primary key (passport#, departure_time),
+       foreign key (passport#) references passengers,
+       foreign key (flight#, departure_time) references airliner_oo2,
+       foreign key (flight#) references airliner_oo1,
+       foreign key (destination) references airport);
 
 grant select on on_board to public;
